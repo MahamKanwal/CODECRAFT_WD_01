beaa0e1fefb614503f38eb63ad57c6487d841a7b
Maham <kanwalmhsm@gmail.com>
2024-12-09 00:08:17 +0500

    navbar

---
+++ app.js
@@ -0,0 +1,12 @@
+// Add scroll event listener to the window
+// window.addEventListener('scroll', () => {
+//     const navbar = document.querySelector('.navbar');
+    
+//     // Check if the page has been scrolled
+//     if (window.scrollY > 50) {
+//       navbar.classList.add('scrolled');
+//     } else {
+//       navbar.classList.remove('scrolled');
+//     }
+//   });
+  
